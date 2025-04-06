452d0ea0d1a904e03fcd1360166fd9532bd04664
Vivek Sharma <vsharma87077@gmail.com>
2025-04-06 22:34:55 +0530

    update

---
+++ Backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import resumeRouter from "./routes/resume.routes.js";
+import cors from "cors";
+import { config } from "dotenv";
+config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+const corsOptions = {
+  origin: [process.env.ALLOWED_SITE],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+
+// Default root route
+app.get("/", (req, res) => {
+    res.send("🚀 Backend is running!");
+  });
+
+export default app;
